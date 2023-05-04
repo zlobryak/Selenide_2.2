ea2434a1e6f0d273bd06bd204a214f2c25c902e3
Neto Homework <clychnik@gmail.com>
2023-05-04 03:28:22 +0300

    Additional task resolved (borderline dates commented out)

--- src/test/java/ry/netology/web/ChromeTest.java
+++ src/test/java/ry/netology/web/ChromeTest.java
@@ -1,35 +1,46 @@
 package ry.netology.web;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
+
 
 import java.time.Duration;
 import java.time.LocalDate;
 
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-import com.codeborne.selenide.SelenideElement;
+
 public class ChromeTest {
     @Test
     void shouldBookCardDeliveryHappyPath() {
 
-//        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = true;
+        int today = LocalDate.now().getDayOfMonth();
+//        int today =31;
+        int selectDay = LocalDate.now().plusWeeks(1).getDayOfMonth();
+//        int selectDay = 6;
+
         open("http://localhost:9999/");
-        SelenideElement block = $("fieldset");
-        block.$("[data-test-id=city] input").sendKeys("Челябинск");
-        block.$(".calendar-input input")
-                .setValue(
-                        String.valueOf(LocalDate.now()
-                                .plusDays(3)
-                        )
-                ).sendKeys(Keys.ESCAPE);
-        block.$("[data-test-id=name] input").setValue("Василий Пупкин");
-        block.$("[data-test-id=phone] input").setValue("+79062421277");
-//        $x("/html/body/div[1]/div/form/fieldset/div[5]/label/span[2]").click();
-        block.$(withText("соглашаюсь")).click();
-        block.$(withText("Забронировать")).click();
+
+        $("[data-test-id=city] input").sendKeys("Че");
+        $(byText("Челябинск")).click();
+        $(".calendar-input input").click();
+
+
+        if (selectDay > today){
+           $(byText(String.valueOf(selectDay))).click();
+        } else {
+            $(".calendar__arrow_direction_right").click();
+            $(byText(String.valueOf(selectDay))).click();
+        }
+        $("[data-test-id=name] input").setValue("Василий Пупкин");
+        $("[data-test-id=phone] input").setValue("+79062421277");
+
+        $(withText("соглашаюсь")).click();
+        $(withText("Забронировать")).click();
 
         $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(15));
     }
